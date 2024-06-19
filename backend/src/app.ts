27d302bfbf1ce29938bdeeaf1ca9cced59fb6e54
pritamdutta50 <pritamdutta50@gmail.com>
2024-06-19 22:20:04 +0530

    2nd deployment trial

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express, { NextFunction, Request, Response } from "express";
 import UserRoute from "./routes/user.route";
 import MessageRoute from "./routes/message.route";
@@ -6,18 +7,25 @@ import cookieParser from "cookie-parser";
 import {app} from "./socket/socket";
 
 export const runApp =() => {
+
+  const __dirname = path.resolve();
   // Middlewares
   app.use(express.json());
   app.use(cookieParser());
   // endpoint declaration
-  app.get("/", (req, res) => {
-    res.send("Hello World");
-  });
+  // app.get("/", (req, res) => {
+  //   res.send("Hello World");
+  // });
   app.use("/api/user", UserRoute);
   app.use("/api/message", MessageRoute);
-  app.use((req, res, next)=>{
-    next(createHttpError(404, "Endpoint not found"));
-  });
+  // app.use((req, res, next)=>{
+  //   next(createHttpError(404, "Endpoint not found"));
+  // });
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  })
 
   // error handling middleware
   app.use((error:unknown, req:Request, res: Response, next: NextFunction)=>{
--- frontend/src/hooks/useListenMessages.ts
+++ frontend/src/hooks/useListenMessages.ts
@@ -2,21 +2,24 @@ import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext"
 import useConversation from "../zustand/useConversation";
 import notificationSound from '../assets/sound/notification.mp3';
+import { MessageModel } from "../models/MessageModel";
 
 const useListenMessages = () => {
   const {socket} = useSocketContext();
-  const {messages, setMessages} = useConversation();
+  const {messages, setMessages, selectedConversation} = useConversation();
 
   useEffect(()=>{
-    socket?.on("newMessage", (message) => {
-        const audio = new Audio(notificationSound);
-        audio.play();
-      setMessages([...messages, message]);
+    socket?.on("newMessage", (message:MessageModel) => {
+      if(message.senderId === selectedConversation?._id){
+          const audio = new Audio(notificationSound);
+          audio.play();
+          setMessages([...messages, message]);
+        }
     });
     return () => {
       socket?.off("newMessage");
     }
-  },[socket, messages, setMessages]);
+  },[socket, messages, setMessages, selectedConversation]);
 }
 
 export default useListenMessages
